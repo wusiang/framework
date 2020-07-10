4dd6a36c12959fb70bc474bcac8211e01403aa8b
map <guyi@maihaoche.com>
2020-07-10 17:36:59 +0800

    完善代码

--- framework/framework-common/src/main/java/com/xianmao/utils/BeanUtil.java
+++ framework/framework-common/src/main/java/com/xianmao/utils/BeanUtil.java
@@ -1,15 +1,17 @@
 package com.xianmao.utils;
 
 import com.xianmao.support.BaseBeanCopier;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.cglib.beans.BeanMap;
 import org.springframework.cglib.core.CodeGenerationException;
 import org.springframework.util.Assert;
 
 import java.beans.PropertyDescriptor;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @ClassName BeanUtil
@@ -120,6 +122,34 @@ public class BeanUtil extends org.springframework.beans.BeanUtils {
     }
 
     /**
+     * 对象转换（空元素不覆盖）
+     *  
+     *
+     * @param source 原对象
+     * @param target 目标对象
+     */
+    public static <T> T copyIgnoreNull(Object source, T target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+        return target;
+    }
+
+    /**
+     * 对象转换
+     *
+     * @param source           原对象
+     * @param target           目标对象
+     * @param ignoreProperties 排除要copy的属性
+     */
+    public static <T> T copy(Object source, T target, String... ignoreProperties) {
+        if (ArrayUtils.isEmpty(ignoreProperties)) {
+            BeanUtils.copyProperties(source, target);
+        } else {
+            BeanUtils.copyProperties(source, target, ignoreProperties);
+        }
+        return target;
+    }
+
+    /**
      * Copy the property values of the given source bean into the target class.
      * <p>Note: The source and target classes do not have to match or even be derived
      * from each other, as long as the properties match. Any bean properties that the
@@ -150,6 +180,47 @@ public class BeanUtil extends org.springframework.beans.BeanUtils {
     }
 
     /**
+     * 将对象装成map形式
+     *
+     * @param bean
+     * @param null2String null是否转空字符串
+     * @param <T>
+     * @return
+     */
+    public static <T> Map<String, Object> toMap(T bean, Boolean null2String) {
+        Map<String, Object> map = new HashMap<>();
+        try {
+            if (bean != null) {
+                BeanMap beanMap = BeanMap.create(bean);
+                map.putAll(beanMap);
+                if (null2String) {
+                    map.replaceAll((k, v) -> v == null ? "" : v);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    /**
+     * 将对象装成map形式
+     *
+     * @param bean
+     * @param null2String      null是否转空字符串
+     * @param ignoreProperties 忽略字符串
+     * @param <T>
+     * @return
+     */
+    public static <T> Map<String, Object> toMap(T bean, Boolean null2String, String... ignoreProperties) {
+        Map<String, Object> map = toMap(bean, null2String);
+        for (String ignoreProperty : ignoreProperties) {
+            map.remove(ignoreProperty);
+        }
+        return map;
+    }
+
+    /**
      * 将map 转为 bean
      *
      * @param beanMap   map
@@ -163,6 +234,7 @@ public class BeanUtil extends org.springframework.beans.BeanUtils {
         return bean;
     }
 
+
     /**
      * 获取 Bean 的所有 get方法
      *
@@ -171,6 +243,26 @@ public class BeanUtil extends org.springframework.beans.BeanUtils {
      */
     public static PropertyDescriptor[] getBeanGetters(Class type) {
         return getPropertiesHelper(type, true, false);
+    }
+
+    /**
+     * 查询对象中空的元素
+     *
+     * @param source 查询对象
+     */
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
 
     /**
