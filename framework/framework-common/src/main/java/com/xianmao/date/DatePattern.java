615a4eedba3132351ab5d6eb826dc7faa8a21b5a
map <guyi@maihaoche.com>
2020-07-14 15:31:36 +0800

    完善代码

--- framework/framework-common/src/main/java/com/xianmao/date/DatePattern.java
+++ framework/framework-common/src/main/java/com/xianmao/date/DatePattern.java
@@ -2,7 +2,9 @@ package com.xianmao.date;
 
 import cn.hutool.core.date.format.FastDateFormat;
 
+import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.logging.SimpleFormatter;
 
 /**
  * @ClassName DatePattern
@@ -21,7 +23,7 @@ public class DatePattern {
     /**
      * 常规格式化 <code>yyyy-MM-dd HH:mm:ss</code>
      */
-    public final static DateTimeFormatter NORM_DATETIME_FORMAT = DateTimeFormatter.ofPattern(NORM_DATETIME_PATTERN);
+    public final static SimpleDateFormat NORM_DATETIME_FORMAT = new SimpleDateFormat(NORM_DATETIME_PATTERN);
 
     /**
      * 常规格式 <code>yyyy-MM-dd</code>
@@ -31,7 +33,7 @@ public class DatePattern {
     /**
      * 常规格式化 <code>yyyy-MM-dd</code>
      */
-    public final static DateTimeFormatter NORM_DATE_FORMAT = DateTimeFormatter.ofPattern(NORM_DATE_PATTERN);
+    public final static SimpleDateFormat NORM_DATE_FORMAT = new SimpleDateFormat(NORM_DATE_PATTERN);
 
     /**
      * 纯净的日期格式 <code>yyyyMMddHHmmss</code>
@@ -41,7 +43,7 @@ public class DatePattern {
     /**
      * 纯净的日期格式化 <code>yyyyMMddHHmmss</code>
      */
-    public final static DateTimeFormatter PURE_ATETIME_FORMAT = DateTimeFormatter.ofPattern(PURE_DATETIME_PATTERN);
+    public final static SimpleDateFormat PURE_ATETIME_FORMAT = new SimpleDateFormat(PURE_DATETIME_PATTERN);
     /**
      * 纯净的日期格式 <code>yyyyMMdd</code>
      */
@@ -50,16 +52,16 @@ public class DatePattern {
     /**
      * 纯净的日期格式化 <code>yyyyMMdd</code>
      */
-    public final static DateTimeFormatter PURE_DATE_FORMAT = DateTimeFormatter.ofPattern(PURE_DATE_PATTERN);
+    public final static SimpleDateFormat PURE_DATE_FORMAT = new SimpleDateFormat(PURE_DATE_PATTERN);
 
     /**
      * 标准日期格式：yyyy年MM月dd日 HH时mm分ss秒
      */
     public static final String CHINESE_DATE_TIME_PATTERN = "yyyy年MM月dd日HH时mm分ss秒";
     /**
-     * 标准日期格式 {@link FastDateFormat}：yyyy年MM月dd日HH时mm分ss秒
+     * 标准日期格式
      */
-    public static final DateTimeFormatter CHINESE_DATE_TIME_FORMAT = DateTimeFormatter.ofPattern(CHINESE_DATE_TIME_PATTERN);
+    public static final SimpleDateFormat CHINESE_DATE_TIME_FORMAT = new SimpleDateFormat(CHINESE_DATE_TIME_PATTERN);
 
     /**
      * 中文的日期格式 <code>yyyy年MM月dd日</code>
@@ -69,7 +71,7 @@ public class DatePattern {
     /**
      * * 中文的日期格式化 <code>yyyy年MM月dd日</code>
      */
-    public final static DateTimeFormatter CHINESE_DATE_FORMAT = DateTimeFormatter.ofPattern(CHINESE_DATE_PATTERN);
+    public final static SimpleDateFormat CHINESE_DATE_FORMAT = new SimpleDateFormat(CHINESE_DATE_PATTERN);
 
     /**
      * 中文的日期格式 <code>HH:mm:ss</code>
@@ -79,5 +81,5 @@ public class DatePattern {
     /**
      * 中文的日期格式 <code>HH:mm:ss</code>
      */
-    public final static DateTimeFormatter PATTERN_TIME_FORMAT = DateTimeFormatter.ofPattern(PATTERN_TIME);
+    public final static SimpleDateFormat PATTERN_TIME_FORMAT = new SimpleDateFormat(PATTERN_TIME);
 }
--- framework/framework-common/src/main/java/com/xianmao/date/DateTimeUtil.java
+++ framework/framework-common/src/main/java/com/xianmao/date/DateTimeUtil.java
@@ -1,20 +1,19 @@
 package com.xianmao.date;
 
-import cn.hutool.core.date.DateTime;
+
 import com.xianmao.string.StringPool;
 import com.xianmao.utils.StringUtil;
 import org.springframework.util.Assert;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
 import java.time.temporal.TemporalAmount;
 import java.time.temporal.TemporalQuery;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
 /**
  * @ClassName DateUtil
@@ -35,24 +34,138 @@ public class DateTimeUtil {
     }
 
     /**
-     * 当前时间的时间戳
+     * 当前时间，格式 yyyy-MM-dd HH:mm:ss
      *
-     * @param isNano 是否为高精度时间
-     * @return 时间
+     * @return 当前时间的标准形式字符串
      */
-    public static long currentTimeMillis(boolean isNano) {
-        return isNano ? System.nanoTime() : System.currentTimeMillis();
+    public static String now() {
+        return formatDateTime(new Date());
     }
 
     /**
-     * 当前时间的时间戳（秒）
+     * 当前日期，格式 yyyy-MM-dd
      *
-     * @return 当前时间秒数
-     * @since 4.0.0
+     * @return 当前日期的标准形式字符串
      */
-    public static long currentSeconds() {
-        return System.currentTimeMillis() / 1000;
+    public static String today() {
+        return formatDate(new Date());
     }
+
+    // ------------------------------------ Format start ----------------------------------------------
+
+    /**
+     * 格式化日期时间<br>
+     * 格式 yyyy-MM-dd HH:mm:ss
+     *
+     * @param localDateTime 被格式化的日期
+     * @return 格式化后的字符串
+     */
+    public static String formatLocalDateTime(LocalDateTime localDateTime) {
+        return format(localDateTime, cn.hutool.core.date.DatePattern.NORM_DATETIME_PATTERN);
+    }
+
+    /**
+     * 根据特定格式格式化日期
+     *
+     * @param localDateTime 被格式化的日期
+     * @param format        日期格式
+     * @return 格式化后的字符串
+     */
+    public static String format(LocalDateTime localDateTime, String format) {
+        if (null == localDateTime || StringUtil.isBlank(format)) {
+            return null;
+        }
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(format);
+        return localDateTime.format(df);
+    }
+
+    /**
+     * 根据特定格式格式化日期
+     *
+     * @param date   被格式化的日期
+     * @param format 日期格式
+     * @return 格式化后的字符串
+     */
+    public static String format(Date date, String format) {
+        if (null == date || StringUtil.isBlank(format)) {
+            return null;
+        }
+        final SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return format(date, sdf);
+    }
+
+    /**
+     * 根据特定格式格式化日期
+     *
+     * @param date   被格式化的日期
+     * @param format {@link SimpleDateFormat}
+     * @return 格式化后的字符串
+     */
+    public static String format(Date date, DateFormat format) {
+        if (null == format || null == date) {
+            return null;
+        }
+        return format.format(date);
+    }
+
+    /**
+     * 根据特定格式格式化日期
+     *
+     * @param date   被格式化的日期
+     * @param format {@link SimpleDateFormat}
+     * @return 格式化后的字符串
+     * @since 5.0.0
+     */
+    public static String format(Date date, DateTimeFormatter format) {
+        if (null == format || null == date) {
+            return null;
+        }
+        return format.format(date.toInstant());
+    }
+
+    /**
+     * 格式化日期时间<br>
+     * 格式 yyyy-MM-dd HH:mm:ss
+     *
+     * @param date 被格式化的日期
+     * @return 格式化后的日期
+     */
+    public static String formatDateTime(Date date) {
+        if (null == date) {
+            return null;
+        }
+        return DatePattern.NORM_DATETIME_FORMAT.format(date);
+    }
+
+    /**
+     * 格式化日期部分（不包括时间）<br>
+     * 格式 yyyy-MM-dd
+     *
+     * @param date 被格式化的日期
+     * @return 格式化后的字符串
+     */
+    public static String formatDate(Date date) {
+        if (null == date) {
+            return null;
+        }
+        return DatePattern.NORM_DATE_FORMAT.format(date);
+    }
+
+    /**
+     * 格式化时间<br>
+     * 格式 HH:mm:ss
+     *
+     * @param date 被格式化的日期
+     * @return 格式化后的字符串
+     * @since 3.0.1
+     */
+    public static String formatTime(Date date) {
+        if (null == date) {
+            return null;
+        }
+        return DatePattern.PATTERN_TIME_FORMAT.format(date);
+    }
+    //
 
     /**
      * 获取某日期当月的天数
@@ -304,50 +417,6 @@ public class DateTimeUtil {
     }
 
     /**
-     * java8 日期时间格式化
-     * 常规格式化 <code>yyyy-MM-dd HH:mm:ss</code>
-     *
-     * @param temporal 时间
-     * @return 格式化后的时间
-     */
-    public static String formatDateTime(TemporalAccessor temporal) {
-        return DatePattern.NORM_DATETIME_FORMAT.format(temporal);
-    }
-
-    /**
-     * java8 日期时间格式化
-     * 常规格式化 <code>yyyy-MM-dd</code>
-     *
-     * @param temporal 时间
-     * @return 格式化后的时间
-     */
-    public static String formatDate(TemporalAccessor temporal) {
-        return DatePattern.NORM_DATE_FORMAT.format(temporal);
-    }
-
-    /**
-     * java8 时间格式化
-     * <code>HH:mm:ss</code>
-     *
-     * @param temporal 时间
-     * @return 格式化后的时间
-     */
-    public static String formatTime(TemporalAccessor temporal) {
-        return DatePattern.PATTERN_TIME_FORMAT.format(temporal);
-    }
-
-    /**
-     * java8 日期格式化
-     *
-     * @param temporal 时间
-     * @param pattern  表达式
-     * @return 格式化后的时间
-     */
-    public static String format(TemporalAccessor temporal, String pattern) {
-        return DateTimeFormatter.ofPattern(pattern).format(temporal);
-    }
-
-    /**
      * 将字符串转换为时间
      *
      * @param dateStr 时间字符串
@@ -425,46 +494,25 @@ public class DateTimeUtil {
         return toMilliseconds(localDate.atStartOfDay());
     }
 
+
     /**
-     * 转换成java8 时间
+     * 当前时间的时间戳
      *
-     * @param calendar 日历
-     * @return LocalDateTime
+     * @param isNano 是否为高精度时间
+     * @return 时间
      */
-    public static LocalDateTime fromCalendar(final Calendar calendar) {
-        TimeZone tz = calendar.getTimeZone();
-        ZoneId zid = tz == null ? ZoneId.systemDefault() : tz.toZoneId();
-        return LocalDateTime.ofInstant(calendar.toInstant(), zid);
+    public static long toMillis(boolean isNano) {
+        return isNano ? System.nanoTime() : System.currentTimeMillis();
     }
 
     /**
-     * 转换成java8 时间
+     * 当前时间的时间戳（秒）
      *
-     * @param instant Instant
-     * @return LocalDateTime
+     * @return 当前时间秒数
+     * @since 4.0.0
      */
-    public static LocalDateTime fromInstant(final Instant instant) {
-        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-    }
-
-    /**
-     * 转换成java8 时间
-     *
-     * @param date Date
-     * @return LocalDateTime
-     */
-    public static LocalDateTime fromDate(final Date date) {
-        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
-    }
-
-    /**
-     * 转换成java8 时间
-     *
-     * @param milliseconds 毫秒数
-     * @return LocalDateTime
-     */
-    public static LocalDateTime fromMilliseconds(final long milliseconds) {
-        return LocalDateTime.ofInstant(Instant.ofEpochMilli(milliseconds), ZoneId.systemDefault());
+    public static long toMilliseconds() {
+        return System.currentTimeMillis() / 1000;
     }
 
     /**
@@ -500,6 +548,7 @@ public class DateTimeUtil {
             return Duration.between(startDate.toInstant(), endDate.toInstant());
         }
     }
+
 
     /**
      * 将秒数转换为日时分秒
@@ -549,7 +598,6 @@ public class DateTimeUtil {
      * @return
      */
     public static long pastHour(Date date) {
-
         long t = System.currentTimeMillis() - date.getTime();
         return t / (60 * 60 * 1000);
     }
