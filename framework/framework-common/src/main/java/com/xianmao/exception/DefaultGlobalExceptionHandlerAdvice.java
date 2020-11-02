50eb5fd2a7a981f888e88a9d028a901b81bc378c
map <guyi@maihaoche.com>
2020-11-02 20:57:56 +0800

    完善异常类

--- framework/framework-common/src/main/java/com/xianmao/exception/DefaultGlobalExceptionHandlerAdvice.java
+++ framework/framework-common/src/main/java/com/xianmao/exception/DefaultGlobalExceptionHandlerAdvice.java
@@ -6,6 +6,7 @@ import com.xianmao.utils.ServletUtil;
 import com.xianmao.utils.WebUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,13 +26,40 @@ public abstract class DefaultGlobalExceptionHandlerAdvice {
     @ExceptionHandler(Exception.class)
     public APIResult<?> handleException(HttpServletRequest req, Exception e) throws IOException {
 
-        logger.error("method:{},header:{},param:{},ip:{},log:{},errorInfo__", WebUtil.getMethodName(req), ServletUtil.getHeaderMap(req).toString(),WebUtil.getRequestParamString(req), WebUtil.getIP(req), e);
+        logger.error("method:{},header:{},param:{},ip:{},log:{},errorInfo__", WebUtil.getMethodName(req), ServletUtil.getHeaderMap(req).toString(), WebUtil.getRequestParamString(req), WebUtil.getIP(req), e);
         if (e instanceof BizException) {
             BizException bizException = (BizException) e;
             return APIResult.fail(bizException.getCode(), bizException.getMsg());
         } else {
             return APIResult.fail(ResultCode.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    /**
+     * 请求方式不支持
+     */
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    public APIResult<?> handleException(HttpServletRequest req, HttpRequestMethodNotSupportedException e) {
+        logger.error("method:{},header:{},param:{},ip:{},log:{},errorInfo__", WebUtil.getMethodName(req), ServletUtil.getHeaderMap(req).toString(), WebUtil.getRequestParamString(req), WebUtil.getIP(req), e);
+        return APIResult.fail("不支持' " + e.getMethod() + "'请求");
+    }
+
+    /**
+     * 拦截未知的运行时异常
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public APIResult<?> notFount(HttpServletRequest req, RuntimeException e) {
+        logger.error("method:{},header:{},param:{},ip:{},log:{},errorInfo__", WebUtil.getMethodName(req), ServletUtil.getHeaderMap(req).toString(), WebUtil.getRequestParamString(req), WebUtil.getIP(req), e);
+        return APIResult.fail("运行时异常:" + e.getMessage());
+    }
+
+    /**
+     * 业务异常
+     */
+    @ExceptionHandler(BizException.class)
+    public Object businessException(HttpServletRequest req, BizException e) {
+        logger.error("method:{},header:{},param:{},ip:{},log:{},errorInfo__", WebUtil.getMethodName(req), ServletUtil.getHeaderMap(req).toString(), WebUtil.getRequestParamString(req), WebUtil.getIP(req), e);
+        return APIResult.fail(e.getMessage());
     }
 
 
