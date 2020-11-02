9c84b7b66762a951b355a6d8d74c78cfaaf4093d
map <guyi@maihaoche.com>
2020-11-02 20:33:08 +0800

    新增重复提交注解

---
+++ framework/framework-common/src/main/java/com/xianmao/annotation/RepeatSubmit.java
@@ -0,0 +1,12 @@
+package com.xianmao.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 自定义注解防止表单重复提交
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RepeatSubmit {
+}
