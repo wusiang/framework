ccd82b82f9dd178a63079b39312ecdc191d04bfb
wusiang <>
2020-12-10 16:38:13 +0800

    完善代码

--- framework/framework-common/src/main/java/com/xianmao/exception/handle/DefaultGlobalExceptionHandler.java
+++ framework/framework-common/src/main/java/com/xianmao/exception/handle/DefaultGlobalExceptionHandler.java
@@ -7,11 +7,15 @@ import com.xianmao.utils.ServletUtil;
 import com.xianmao.utils.WebUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.net.BindException;
 
 /**
  * @ClassName BaseExceptionHandler
@@ -34,6 +38,24 @@ public abstract class DefaultGlobalExceptionHandler {
         } else {
             return APIResult.fail(ResultCode.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    /**
+     * 校验异常
+     */
+    @ExceptionHandler(value = BindException.class)
+    public APIResult<?> validationExceptionHandler(HttpServletRequest req, BindException e) {
+        logger.error("method:{},header:{},param:{},ip:{},log:{},errorInfo__", WebUtil.getMethodName(req), ServletUtil.getHeaderMap(req).toString(), WebUtil.getRequestParamString(req), WebUtil.getIP(req), e);
+        return APIResult.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+
+    /**
+     * 校验异常
+     */
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public APIResult<?> handleMissingServletRequestParameterException(HttpServletRequest req, MissingServletRequestParameterException e) {
+        logger.error("method:{},header:{},param:{},ip:{},log:{},errorInfo__", WebUtil.getMethodName(req), ServletUtil.getHeaderMap(req).toString(), WebUtil.getRequestParamString(req), WebUtil.getIP(req), e);
+        return APIResult.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 
     /**
