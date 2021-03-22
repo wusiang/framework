9e1226926339cb51f90a4b18ceb77d852e641895
wusiang <>
2021-03-22 13:26:24 +0800

    晚上重复提交

--- framework/framework-common/src/main/java/com/xianmao/annotation/RepeatSubmit.java
+++ framework/framework-common/src/main/java/com/xianmao/annotation/RepeatSubmit.java
@@ -9,4 +9,13 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface RepeatSubmit {
+    /**
+     *  重复提交、缓存时间设置 参数毫秒
+     */
+    long time() default 3000L;
+
+    /**
+     * 当重复提交异常是否抛出，默认是false 缓存请求信息  true即抛出异常
+     */
+    boolean throwable() default false;
 }
