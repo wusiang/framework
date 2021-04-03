12115cf519fe36eac723d973dfd435317715eea9
wusiang <>
2021-04-03 19:17:36 +0800

    添加工具

---
+++ framework/framework-common/src/main/java/com/xianmao/utils/LocalDateTimeUtils.java
@@ -0,0 +1,335 @@
+package com.xianmao.utils;
+
+import com.xianmao.obj.ObjectUtil;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.*;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class LocalDateTimeUtils {
+
+    /**
+     * 当前时间，默认时区
+     *
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime now() {
+        return LocalDateTime.now();
+    }
+
+    /**
+     * {@link Instant}转{@link LocalDateTime}，使用默认时区
+     *
+     * @param instant {@link Instant}
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(Instant instant) {
+        return of(instant, ZoneId.systemDefault());
+    }
+
+    /**
+     * {@link Instant}转{@link LocalDateTime}，使用UTC时区
+     *
+     * @param instant {@link Instant}
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime ofUTC(Instant instant) {
+        return of(instant, ZoneId.of("UTC"));
+    }
+
+    /**
+     * {@link ZonedDateTime}转{@link LocalDateTime}
+     *
+     * @param zonedDateTime {@link ZonedDateTime}
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(ZonedDateTime zonedDateTime) {
+        if (null == zonedDateTime) {
+            return null;
+        }
+        return zonedDateTime.toLocalDateTime();
+    }
+
+    /**
+     * {@link Instant}转{@link LocalDateTime}
+     *
+     * @param instant {@link Instant}
+     * @param zoneId  时区
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(Instant instant, ZoneId zoneId) {
+        if (null == instant) {
+            return null;
+        }
+
+        return LocalDateTime.ofInstant(instant, ObjectUtil.defaultIfNull(zoneId, ZoneId.systemDefault()));
+    }
+
+    /**
+     * {@link Instant}转{@link LocalDateTime}
+     *
+     * @param instant  {@link Instant}
+     * @param timeZone 时区
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(Instant instant, TimeZone timeZone) {
+        if (null == instant) {
+            return null;
+        }
+
+        return of(instant, ObjectUtil.defaultIfNull(timeZone, TimeZone.getDefault()).toZoneId());
+    }
+
+    /**
+     * 毫秒转{@link LocalDateTime}，使用默认时区
+     *
+     * <p>注意：此方法使用默认时区，如果非UTC，会产生时间偏移</p>
+     *
+     * @param epochMilli 从1970-01-01T00:00:00Z开始计数的毫秒数
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(long epochMilli) {
+        return of(Instant.ofEpochMilli(epochMilli));
+    }
+
+    /**
+     * 毫秒转{@link LocalDateTime}，使用UTC时区
+     *
+     * @param epochMilli 从1970-01-01T00:00:00Z开始计数的毫秒数
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime ofUTC(long epochMilli) {
+        return ofUTC(Instant.ofEpochMilli(epochMilli));
+    }
+
+    /**
+     * 毫秒转{@link LocalDateTime}，根据时区不同，结果会产生时间偏移
+     *
+     * @param epochMilli 从1970-01-01T00:00:00Z开始计数的毫秒数
+     * @param zoneId     时区
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(long epochMilli, ZoneId zoneId) {
+        return of(Instant.ofEpochMilli(epochMilli), zoneId);
+    }
+
+    /**
+     * 毫秒转{@link LocalDateTime}，结果会产生时间偏移
+     *
+     * @param epochMilli 从1970-01-01T00:00:00Z开始计数的毫秒数
+     * @param timeZone   时区
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(long epochMilli, TimeZone timeZone) {
+        return of(Instant.ofEpochMilli(epochMilli), timeZone);
+    }
+
+    /**
+     * {@link Date}转{@link LocalDateTime}，使用默认时区
+     *
+     * @param date Date对象
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(Date date) {
+        if (null == date) {
+            return null;
+        }
+        return of(date.toInstant());
+    }
+
+
+    /**
+     * 解析日期时间字符串为{@link LocalDateTime}，仅支持yyyy-MM-dd'T'HH:mm:ss格式，例如：2007-12-03T10:15:30
+     *
+     * @param text 日期时间字符串
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime parse(CharSequence text) {
+        return parse(text, (DateTimeFormatter) null);
+    }
+
+    /**
+     * 解析日期时间字符串为{@link LocalDateTime}，格式支持日期时间、日期、时间
+     *
+     * @param text      日期时间字符串
+     * @param formatter 日期格式化器，预定义的格式见：{@link DateTimeFormatter}
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime parse(CharSequence text, DateTimeFormatter formatter) {
+        if (null == text) {
+            return null;
+        }
+        if (null == formatter) {
+            return LocalDateTime.parse(text);
+        }
+
+        return of(formatter.parse(text));
+    }
+
+    /**
+     * 日期偏移,根据field不同加不同值（偏移会修改传入的对象）
+     *
+     * @param time   {@link LocalDateTime}
+     * @param number 偏移量，正数为向后偏移，负数为向前偏移
+     * @param field  偏移单位，见{@link ChronoUnit}，不能为null
+     * @return 偏移后的日期时间
+     */
+    public static LocalDateTime offset(LocalDateTime time, long number, TemporalUnit field) {
+        if (null == time) {
+            return null;
+        }
+
+        return time.plus(number, field);
+    }
+
+    /**
+     * 获取两个日期的差，如果结束时间早于开始时间，获取结果为负。
+     * <p>
+     * 返回结果为{@link Duration}对象，通过调用toXXX方法返回相差单位
+     *
+     * @param startTimeInclude 开始时间（包含）
+     * @param endTimeExclude   结束时间（不包含）
+     * @return 时间差 {@link Duration}对象
+     */
+    public static Duration between(LocalDateTime startTimeInclude, LocalDateTime endTimeExclude) {
+        return between(startTimeInclude, endTimeExclude);
+    }
+
+    /**
+     * 获取两个日期的差，如果结束时间早于开始时间，获取结果为负。
+     * <p>
+     * 返回结果为时间差的long值
+     *
+     * @param startTimeInclude 开始时间（包括）
+     * @param endTimeExclude   结束时间（不包括）
+     * @param unit             时间差单位
+     * @return 时间差
+     * @since 5.4.5
+     */
+    public static long between(LocalDateTime startTimeInclude, LocalDateTime endTimeExclude, ChronoUnit unit) {
+        return between(startTimeInclude, endTimeExclude, unit);
+    }
+
+    /**
+     * 获取两个日期的表象时间差，如果结束时间早于开始时间，获取结果为负。
+     * <p>
+     * 比如2011年2月1日，和2021年8月11日，日相差了10天，月相差6月
+     *
+     * @param startTimeInclude 开始时间（包括）
+     * @param endTimeExclude   结束时间（不包括）
+     * @return 时间差
+     * @since 5.4.5
+     */
+    public static Period betweenPeriod(LocalDate startTimeInclude, LocalDate endTimeExclude) {
+        return Period.between(startTimeInclude, endTimeExclude);
+    }
+
+    /**
+     * 获取两个日期的差，如果结束时间早于开始时间，获取结果为负。
+     * <p>
+     * 返回结果为{@link Duration}对象，通过调用toXXX方法返回相差单位
+     *
+     * @param startTimeInclude 开始时间（包含）
+     * @param endTimeExclude   结束时间（不包含）
+     * @return 时间差 {@link Duration}对象
+     */
+    private static Duration between(Temporal startTimeInclude, Temporal endTimeExclude) {
+        return Duration.between(startTimeInclude, endTimeExclude);
+    }
+
+    /**
+     * 获取两个日期的差，如果结束时间早于开始时间，获取结果为负。
+     * <p>
+     * 返回结果为时间差的long值
+     *
+     * @param startTimeInclude 开始时间（包括）
+     * @param endTimeExclude   结束时间（不包括）
+     * @param unit             时间差单位
+     * @return 时间差
+     */
+    private static long between(Temporal startTimeInclude, Temporal endTimeExclude, ChronoUnit unit) {
+        return unit.between(startTimeInclude, endTimeExclude);
+    }
+
+    /**
+     * 格式化日期时间为指定格式
+     *
+     * @param time      {@link TemporalAccessor}
+     * @param formatter 日期格式化器，预定义的格式见：{@link DateTimeFormatter}
+     * @return 格式化后的字符串
+     * @since 5.3.10
+     */
+    public static String format(TemporalAccessor time, DateTimeFormatter formatter) {
+        if (null == time) {
+            return null;
+        }
+
+        if (null == formatter) {
+            formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        }
+
+
+        try {
+            return formatter.format(time);
+        } catch (UnsupportedTemporalTypeException e) {
+            if (time instanceof LocalDate && e.getMessage().contains("HourOfDay")) {
+                // 用户传入LocalDate，但是要求格式化带有时间部分，转换为LocalDateTime重试
+                return formatter.format(((LocalDate) time).atStartOfDay());
+            } else if (time instanceof LocalTime && e.getMessage().contains("YearOfEra")) {
+                // 用户传入LocalTime，但是要求格式化带有日期部分，转换为LocalDateTime重试
+                return formatter.format(((LocalTime) time).atDate(LocalDate.now()));
+            }
+            throw e;
+        }
+    }
+
+    /**
+     * 安全获取时间的某个属性，属性不存在返回0
+     *
+     * @param temporalAccessor 需要获取的时间对象
+     * @param field            需要获取的属性
+     * @return 时间的值，如果无法获取则默认为 0
+     */
+    public static int get(TemporalAccessor temporalAccessor, TemporalField field) {
+        if (temporalAccessor.isSupported(field)) {
+            return temporalAccessor.get(field);
+        }
+
+        return (int) field.range().getMinimum();
+    }
+
+    /**
+     * {@link TemporalAccessor}转{@link LocalDateTime}，使用默认时区
+     *
+     * @param temporalAccessor {@link TemporalAccessor}
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime of(TemporalAccessor temporalAccessor) {
+        if (null == temporalAccessor) {
+            return null;
+        }
+
+        if (temporalAccessor instanceof LocalDate) {
+            return ((LocalDate) temporalAccessor).atStartOfDay();
+        }
+
+        return LocalDateTime.of(
+                get(temporalAccessor, ChronoField.YEAR),
+                get(temporalAccessor, ChronoField.MONTH_OF_YEAR),
+                get(temporalAccessor, ChronoField.DAY_OF_MONTH),
+                get(temporalAccessor, ChronoField.HOUR_OF_DAY),
+                get(temporalAccessor, ChronoField.MINUTE_OF_HOUR),
+                get(temporalAccessor, ChronoField.SECOND_OF_MINUTE),
+                get(temporalAccessor, ChronoField.NANO_OF_SECOND)
+        );
+    }
+}
