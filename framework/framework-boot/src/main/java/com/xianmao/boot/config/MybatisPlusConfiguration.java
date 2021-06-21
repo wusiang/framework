c87ca458cd782148c454b385dd5a0b0c4e0ece02
wusiang <>
2021-06-21 13:23:29 +0800

    完善mybatisplus插件

--- framework/framework-boot/src/main/java/com/xianmao/boot/config/MybatisPlusConfiguration.java
+++ framework/framework-boot/src/main/java/com/xianmao/boot/config/MybatisPlusConfiguration.java
@@ -1,14 +1,22 @@
 package com.xianmao.boot.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.xianmao.boot.mybatis.plugins.SqlFilterArgumentResolver;
 import com.xianmao.boot.mybatis.plugins.SqlLogInterceptor;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration()
 @AllArgsConstructor
-public class MybatisPlusConfiguration {
+public class MybatisPlusConfiguration implements WebMvcConfigurer {
 
 
     /**
@@ -23,5 +31,24 @@ public class MybatisPlusConfiguration {
     @ConditionalOnProperty(value = "xianmao.mybatis-plus.sql-log", matchIfMissing = true)
     public SqlLogInterceptor sqlLogInterceptor() {
         return new SqlLogInterceptor();
+    }
+
+    /**
+     * SQL 过滤器避免SQL 注入
+     * @param argumentResolvers
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SqlFilterArgumentResolver());
+    }
+
+    /**
+     * 分页插件, 对于单一数据库类型来说,都建议配置该值,避免每次分页都去抓取数据库类型
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
