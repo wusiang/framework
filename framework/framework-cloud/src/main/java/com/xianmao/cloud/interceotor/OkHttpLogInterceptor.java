ed47ff15e778216c7fd049823bdf06fe69684750
wusiang <>
2021-09-10 22:41:16 +0800

    完善代码

--- framework/framework-cloud/src/main/java/com/xianmao/cloud/interceotor/OkHttpLogInterceptor.java
+++ framework/framework-cloud/src/main/java/com/xianmao/cloud/interceotor/OkHttpLogInterceptor.java
@@ -5,6 +5,7 @@ import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
@@ -12,6 +13,7 @@ import java.io.IOException;
  *
  */
 @Slf4j
+@Component
 public class OkHttpLogInterceptor implements Interceptor {
 
 
