3e4f217424fafeb358eeb43925330588fa01023e
wujh <wujh>
2022-11-03 14:38:48 +0800

    调整baseEntity

--- framework-common-core/src/main/java/com/xianmao/common/core/exception/DefaultGlobalExceptionHandler.java
+++ framework-common-core/src/main/java/com/xianmao/common/core/exception/DefaultGlobalExceptionHandler.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
@@ -55,6 +57,14 @@ public class DefaultGlobalExceptionHandler {
     public APIResult<?> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
         logger.error(Error.buildError(ServerErrorCode.INTERNAL_SERVER_ERROR.getCode(), ExceptionUtils.getExceptionString(e)));
         return APIResult.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public APIResult<?> MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        logger.error(Error.buildError(ServerErrorCode.INTERNAL_SERVER_ERROR.getCode(), ExceptionUtils.getExceptionString(e)), e);
+        assert fieldError != null;
+        return APIResult.fail(HttpStatus.BAD_REQUEST.value(), fieldError.getDefaultMessage());
     }
 
     /**
