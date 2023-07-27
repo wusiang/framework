2dcb88a3c427f5d824b18db6f52119bd2084384b
wujh <1004987192@qq.com>
2023-07-27 13:39:12 +0800

    中划线

--- framework-common-entity/src/main/java/com/xianmao/common/entity/constants/TokenConstants.java
+++ framework-common-entity/src/main/java/com/xianmao/common/entity/constants/TokenConstants.java
@@ -2,9 +2,9 @@ package com.xianmao.common.entity.constants;
 
 public class TokenConstants {
     /*** 获取到的凭证 */
-    public static final String ACCESS_TOKEN = "access_token";
+    public static final String ACCESS_TOKEN = "access-token";
     /*** 获取到的刷新凭证 */
-    public static final String REFRESH_TOKEN = "refresh_token";
+    public static final String REFRESH_TOKEN = "refresh-token";
     /*** 凭证有效时间，单位：秒 */
-    public static final String expires_in = "expires_in";
+    public static final String expires_in = "expires-in";
 }
