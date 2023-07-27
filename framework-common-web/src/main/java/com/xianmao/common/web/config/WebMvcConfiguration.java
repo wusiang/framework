fef36a0ea6ba15b16bd4a00ae69fcf6890dbdbe9
wujh <1004987192@qq.com>
2023-07-27 13:35:13 +0800

    删除跨越配置

--- framework-common-web/src/main/java/com/xianmao/common/web/config/WebMvcConfiguration.java
+++ framework-common-web/src/main/java/com/xianmao/common/web/config/WebMvcConfiguration.java
@@ -26,19 +26,6 @@ import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebA
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     /**
-     * 跨域配置
-     * 通过重写 addCorsMappings 方法实现跨域配置的支持，使用 CorsRegistry 注册类添加路径映射。示例代码如下：
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")//配置允许跨域的路径
-                .allowedOrigins("*")//配置允许访问的跨域资源的请求域名
-                .allowedMethods("PUT,POST,GET,DELETE,OPTIONS")//配置允许访问该跨域资源服务器的请求方法，如：POST、GET、PUT、DELETE等
-                .allowedHeaders("*"); //配置允许请求header的访问，如 ：X-TOKEN
-        super.addCorsMappings(registry);
-    }
-
-    /**
      * 增加GET请求参数中时间类型转换
      * <ul>
      * <li>HH:mm:ss -> LocalTime</li>
