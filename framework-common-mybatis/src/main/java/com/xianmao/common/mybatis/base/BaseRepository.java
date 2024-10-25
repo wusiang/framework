13451725c9c230075905258032fdbf8cf5c71e75
wujh <wujinghuajinghua@dingtalk.com>
2024-10-25 13:14:56 +0800

    feat: 升级mybatisplus 3.5.9 适配CrudRepository

---
+++ framework-common-mybatis/src/main/java/com/xianmao/common/mybatis/base/BaseRepository.java
@@ -0,0 +1,13 @@
+package com.xianmao.common.mybatis.base;
+
+import com.baomidou.mybatisplus.extension.repository.IRepository;
+
+/**
+ * 基础业务接口
+ *
+ * @param <T>
+ * @author Chill
+ */
+public interface BaseRepository<T> extends IRepository<T> {
+
+}
