36447e5957e1c28c9920a85370c4f633b6d785ff
wujh <1004987192@qq.com>
2024-11-29 15:49:24 +0800

    新增环境变量

---
+++ framework-common-entity/src/main/java/com/xianmao/common/entity/constants/AppConstant.java
@@ -0,0 +1,20 @@
+package com.xianmao.common.entity.constants;
+
+public class AppConstant {
+    /**
+     * 开发环境
+     */
+    String DEV_CODE = "dev";
+    /**
+     * 生产环境
+     */
+    String PROD_CODE = "prod";
+    /**
+     * 测试环境
+     */
+    String TEST_CODE = "test";
+    /**
+     * 预发环境
+     */
+    String PRE_CODE = "pre";
+}
