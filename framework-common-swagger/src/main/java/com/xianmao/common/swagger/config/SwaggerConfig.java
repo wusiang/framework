f36373aaca97b65e3fdcb5ceac4ce5997df9cb21
wujh <wujinghuajinghua@dingtalk.com>
2025-01-21 17:36:20 +0800

    全局参数

--- framework-common-swagger/src/main/java/com/xianmao/common/swagger/config/SwaggerConfig.java
+++ framework-common-swagger/src/main/java/com/xianmao/common/swagger/config/SwaggerConfig.java
@@ -6,9 +6,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+
+import java.util.logging.Logger;
 
 /***
  * 创建Swagger配置
@@ -16,10 +20,12 @@ import org.springframework.context.annotation.Bean;
 @AutoConfiguration
 public class SwaggerConfig {
 
+    private Logger log = Logger.getLogger(this.getClass().getName());
+
     @Bean
     @ConditionalOnMissingBean(OpenAPI.class)
     public OpenAPI swaggerOpenAPI() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info().title("SwaggerOpenAPI接口文档")
                         // 信息
                         .contact(new Contact().name("作者").email("邮箱").url("地址"))
@@ -31,6 +37,13 @@ public class SwaggerConfig {
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
                         .description("外部文档")
-                        .url("https://springshop.wiki.github.org/docs"));
+                        .url("https://springshop.wiki.github.org/docs"))
+                // 接口全局
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("x-request-id")
+                        .addList("x-request-token")
+                        .addList("x-request-sign")
+                        .addList("x-request-timestamp"));
+        return openAPI;
     }
 }
