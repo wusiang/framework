7fd79fc07f89ff48e03bb581a8bafc3a259b6d55
wujh <wujinghuajinghua@dingtalk.com>
2025-01-24 14:53:25 +0800

    ienum 添加getByCode

--- framework-common-core/src/main/java/com/xianmao/common/core/enums/IEnum.java
+++ framework-common-core/src/main/java/com/xianmao/common/core/enums/IEnum.java
@@ -1,6 +1,7 @@
 package com.xianmao.common.core.enums;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public interface IEnum<C, V> extends Serializable {
 
@@ -17,4 +18,18 @@ public interface IEnum<C, V> extends Serializable {
      * @return value 值
      */
     V getValue();
+
+
+    /**
+     * 通用的根据 code 查询枚举对象的方法
+     */
+    public static <E extends Enum<E> & IEnum<C, V>, C, V> E getByCode(C code, Class<E> eClass) {
+        if (code == null || eClass == null) {
+            return null;
+        }
+        return Arrays.stream(eClass.getEnumConstants())
+                .filter(e -> e.getCode().equals(code))
+                .findFirst()
+                .orElse(null);
+    }
 }
