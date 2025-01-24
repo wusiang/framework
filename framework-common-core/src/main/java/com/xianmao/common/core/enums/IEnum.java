9795cfef708e5a1f487884b691abd5a22287e2b8
wujh <wujinghuajinghua@dingtalk.com>
2025-01-24 14:12:47 +0800

    去除IEnum错误方法

--- framework-common-core/src/main/java/com/xianmao/common/core/enums/IEnum.java
+++ framework-common-core/src/main/java/com/xianmao/common/core/enums/IEnum.java
@@ -1,13 +1,6 @@
 package com.xianmao.common.core.enums;
 
-import cn.hutool.core.util.ObjectUtil;
-
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public interface IEnum<C, V> extends Serializable {
 
@@ -24,34 +17,4 @@ public interface IEnum<C, V> extends Serializable {
      * @return value 值
      */
     V getValue();
-
-    /**
-     * 获取指定枚举类的所有枚举值
-     *
-     * @param <E>   枚举类型
-     * @param clazz 枚举类的 Class 对象
-     * @return 包含所有枚举值的列表
-     */
-    static <E extends Enum<E>> List<E> getAllEnums(Class<E> clazz) {
-        // 确保传入的枚举类不为空
-        Objects.requireNonNull(clazz, "枚举类不能为 null");
-        return Arrays.stream(clazz.getEnumConstants()).collect(Collectors.toList());
-    }
-
-    /**
-     * 根据值获取对应的枚举实例
-     *
-     * @param <E>   枚举类型，该枚举类型必须实现 IEnum 接口
-     * @param value 用于匹配的 value 值
-     * @param clazz 枚举类的 Class 对象
-     * @return 匹配到的枚举实例的 Optional 对象，如果未匹配到则返回空的 Optional
-     */
-    static <E extends Enum<E> & IEnum> Optional<E> fromValue(Object value, Class<E> clazz) {
-        // 确保传入的 value 和枚举类都不为空
-        Objects.requireNonNull(value, "value 不能为 null");
-        Objects.requireNonNull(clazz, "枚举类不能为 null");
-        return Arrays.stream(clazz.getEnumConstants())
-                .filter(e -> ObjectUtil.equal(e.getValue(), value))
-                .findFirst();
-    }
 }
